f5350fe97e86a9ddd934795e560c26ecc4ffc031
kevin <thebounstar@gmail.com>
2018-08-01 20:28:40 +1000

    test'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Sphere from '../components/sphere'
 
+import styles from './index.module.scss'
+
 class IndexPage extends React.Component {
 
   render() {
     const { transition } = this.props;
 
     return (
-      <div style={transition && transition.style}>
+      <div style={transition && transition.style} className={styles.container}>
         <Sphere />
       </div>
     );
