d8146f74bb22e52bda6e0232b2eb7c09e79efbeb
kevin <thebounstar@gmail.com>
2018-08-12 13:43:35 +1000

    fun vertical slider

--- src/pages/fun/index.js
+++ src/pages/fun/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import HorizontalScroll from 'react-scroll-horizontal'
 
 import styles from './fun.module.scss'
 
-const FunPage = ({ transition }) => (
-  <div style={transition && transition.style}>
-  </div>
-)
+class FunPage extends Component {
+  render() {
+    const transition = this.props
+
+    return (
+      <div style={transition && transition.style} className={styles.container}>
+        <HorizontalScroll className={styles.alignCenter}>
+          <div className={styles.funCard}></div>
+          <div className={styles.funCard}></div>
+          <div className={styles.funCard}></div>
+          <div className={styles.funCard}></div>
+          <div className={styles.funCard}></div>
+          <div className={styles.funCard}></div>
+          <div className={styles.funCard}></div>
+          <div className={styles.funCard}></div>
+        </HorizontalScroll>
+      </div>
+    )
+  }
+}
 
 export default FunPage
---
+++ src/pages/projects/template.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+import styles from './template.module.scss'
+
+const ProjectTemplate = () => (
+    <Fragment>
+        <div className={styles.titleContainer}>
+        </div>
+    </Fragment>
+)
+
+export default ProjectTemplate
