ddb58d325855507689e84384a9f899a97b5e5c6c
kevin <thebounstar@gmail.com>
2018-08-12 19:26:36 +1000

    ssr fix

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -5,7 +5,7 @@ import rootReducer from './reducers';
 export default () => {
   const middleware = [thunk];
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   // const devtools =
   //   process.env.NODE_ENV === 'development' && window.devToolsExtension
   //     ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
