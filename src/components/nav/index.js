64867c56b0f60cc98fecd4fdc04db3a17eb3424d
kevin <thebounstar@gmail.com>
2018-08-23 22:03:40 +1000

    mobile

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -5,24 +5,7 @@ import Button from '../button'
 import styles from './index.module.scss'
 
 class Nav extends React.Component {
-    state = {
-        open: false
-    }
-
-    constructor(props) {
-        super(props)
-
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    handleClick() {
-        this.setState({ open: !this.state.open })
-    }
-
     render() {
-
-        const { open } = this.state;
-
         return (
             <Fragment>
 
@@ -39,18 +22,17 @@ class Nav extends React.Component {
                     <Link to='/info' activeClassName={styles.activeLink}>Info</Link>
                 </nav>
                 <div className={styles.bottomLeft}>
-                    <Button link='mailto:nveronicatran@gmail.com'>nveronicatran@gmail.com</Button>
+                    <Button link='mailto:nveronicatran@gmail.com' className={styles.hideMobile}>nveronicatran@gmail.com</Button>
+                    <Link to='/fun' activeClassName={styles.activeLink}>Fun</Link>
+                </div>
+
+                <div className={styles.bottomRight}>
+                    <Link to='/projects' activeClassName={styles.activeLink}>Projects</Link>
                 </div>
 
                 <div className={styles.mobileNav}>
                     <Button link='/'>Veronica Tran</Button>
-                    <Button onClick={this.handleClick}>Menu</Button>
-                </div>
-
-                <div className={open ? styles.mobileNavItemsOpen : styles.mobileNavItems}>
-                    <Link to='/projects' activeClassName={styles.activeLink} onClick={this.handleClick}>Projects</Link>
-                    <Link to='/fun' activeClassName={styles.activeLink} onClick={this.handleClick}>Fun</Link>
-                    <Link to='/info' activeClassName={styles.activeLink} onClick={this.handleClick}>Info</Link>
+                    <Link to='/info' activeClassName={styles.activeLink}>Info</Link>
                 </div>
             </Fragment>
         );
