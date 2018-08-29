02c205347433802820d9103c93f82537097cdd3f
kevin <thebounstar@gmail.com>
2018-08-29 20:12:28 +1000

    prev/next

--- src/pages/projects/contemporary-hotels.js
+++ src/pages/projects/contemporary-hotels.js
@@ -9,7 +9,7 @@ import styles from './template.module.scss'
 class ContemporaryHotelsPage extends React.Component {
 
   componentDidMount() {
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
   }
 
   render() {
@@ -25,21 +25,32 @@ class ContemporaryHotelsPage extends React.Component {
               <p className={styles.subtitle}>UX, UI, ILLUSTRATION, MOTION</p>
               <p className={styles.subtitle}>AGENCY: LIQUID</p>
             </div>
-  
+
             <div className={styles.contentContainer}>
               <p><b>Luxurious hotel booking experience</b></p>
               <p>Enhanced digital presence for Sydney based luxury accommodation specialists 'Contemporary Hotels', utilising a comprehensive property search, alongside a high-end showcase of their homes.</p>
             </div>
           </section>
         </div>
-  
+
         <Img fluid={data.galleryImage1.childImageSharp.fluid} className={styles.placeholder} />
         <Img fluid={data.galleryImage2.childImageSharp.fluid} className={styles.placeholder} />
         <Img fluid={data.galleryImage4.childImageSharp.fluid} className={styles.placeholder} />
         <Img fluid={data.galleryImage5.childImageSharp.fluid} className={styles.placeholder} />
         <Img fluid={data.galleryImage6.childImageSharp.fluid} className={styles.placeholder} />
+
+        <div className={styles.thumbnailContainer}>
+          <div>
+            <p>Carazo</p>
+            <Img fixed={data.thumbnailPrev.childImageSharp.fixed} className={styles.thumbnail} />
+          </div>
+          <div>
+            <p>Carazo</p>
+            <Img fixed={data.thumbnailNext.childImageSharp.fixed} className={styles.thumbnail} />
+          </div>
+        </div>
       </div>
-  );
+    );
   }
 }
 
@@ -49,36 +60,50 @@ export const query = graphql`
 query {
   galleryImage1: file(relativePath: { eq: "ch/ch-1.jpg" }) {
     childImageSharp {
-      fluid(maxWidth: 3000, quality: 70) {
+      fluid(maxWidth: 3000, quality: 100) {
         ...GatsbyImageSharpFluid_withWebp_noBase64
       }
     }
   }
   galleryImage2: file(relativePath: { eq: "ch/ch-2.jpg" }) {
     childImageSharp {
-      fluid(maxWidth: 3000, quality: 70) {
+      fluid(maxWidth: 3000, quality: 100) {
         ...GatsbyImageSharpFluid_withWebp_noBase64
       }
     }
   }
   galleryImage4: file(relativePath: { eq: "ch/ch-4.jpg" }) {
     childImageSharp {
-      fluid(maxWidth: 3000, quality: 70) {
+      fluid(maxWidth: 3000, quality: 100) {
         ...GatsbyImageSharpFluid_withWebp_noBase64
       }
     }
   }
   galleryImage5: file(relativePath: { eq: "ch/ch-5.jpg" }) {
     childImageSharp {
-      fluid(maxWidth: 3000, quality: 70) {
+      fluid(maxWidth: 3000, quality: 100) {
         ...GatsbyImageSharpFluid_withWebp_noBase64
       }
     }
   }
   galleryImage6: file(relativePath: { eq: "ch/ch-6.jpg" }) {
     childImageSharp {
-      fluid(maxWidth: 3000, quality: 70) {
+      fluid(maxWidth: 3000, quality: 100) {
         ...GatsbyImageSharpFluid_withWebp_noBase64
+      }
+    }
+  }
+  thumbnailNext: file(relativePath: { eq: "thumbnails/fc-feature-thumbnail.png" }) {
+    childImageSharp {
+      fixed(width: 400, quality: 100) {
+        ...GatsbyImageSharpFixed_withWebp_noBase64
+      }
+    }
+  }
+  thumbnailPrev: file(relativePath: { eq: "thumbnails/fc-feature-thumbnail.png" }) {
+    childImageSharp {
+      fixed(width: 400, quality: 100) {
+        ...GatsbyImageSharpFixed_withWebp_noBase64
       }
     }
   }
