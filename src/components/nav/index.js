50e4e07aa0a1f51108d17166991d5fc4851a79ab
kevin <thebounstar@gmail.com>
2018-09-02 15:27:35 +1000

    scroll ting

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -12,11 +12,13 @@ import styles from './index.module.scss'
 class Nav extends React.Component {
     state = {
         scrollDirection: 'down',
-        scrollPos: 0
+        hasScrolled: false
     }
 
     constructor(props) {
         super(props);
+
+        this.scrollPos = window.pageYOffset || 0;
 
         this.handleScroll = this.handleScroll.bind(this);
     }
@@ -26,7 +28,20 @@ class Nav extends React.Component {
     }
 
     handleScroll() {
-        this.setState({ scrollPos: window.pageYOffset, scrollDirection: window.pageYOffset < this.state.scrollPos ? 'up' : 'down' });
+        if (!this.state.hasScrolled) this.setState({ hasScrolled: true });
+
+        const nextScrollPos = window.pageYOffset;
+        const scrollPos = this.scrollPos;
+
+        if (nextScrollPos > scrollPos) {
+            if (this.state.scrollDirection === 'up') this.setState({ scrollDirection: 'down' });
+        }
+
+        if (nextScrollPos < scrollPos) {
+            if (this.state.scrollDirection === 'down') this.setState({ scrollDirection: 'up' });
+        }
+
+        this.scrollPos = nextScrollPos;
     }
 
     componentDidMount() {
@@ -35,7 +50,7 @@ class Nav extends React.Component {
         window.addEventListener('scroll', this.handleScroll, {
             capture: true,
             passive: true
-          });
+        });
     }
 
     componentWillUnmount() {
@@ -44,14 +59,15 @@ class Nav extends React.Component {
 
     render() {
 
-        const { scrollPos, scrollDirection } = this.state;
+        const { scrollDirection, hasScrolled } = this.state;
         const { newGame } = this.props.appActions;
+        console.log(hasScrolled);
 
         return (
             <Consumer>
                 {({ setActiveWindow }) => (
                     <Fragment>
-                        <nav className={`${styles.verticalNav} ${scrollPos > 0 && scrollDirection === 'down' ? styles.navTopHidden : styles.navTopActive}`}>
+                        <nav className={`${styles.verticalNav} ${hasScrolled && scrollDirection === 'down' ? styles.navTopHidden : styles.navTopActive}`}>
                             <div className={`${styles.navLinks} ${styles.hideMobile}`}>
                                 <Link to='/projects' activeClassName={styles.activeLink}>Projects</Link>
                                 <Link to='/fun' activeClassName={styles.activeLink}>Fun</Link>
@@ -61,10 +77,10 @@ class Nav extends React.Component {
                                 <Button link='/'>Veronica Tran</Button>
                             </div>
                             <div className={styles.topRight}>
-                            <Link to='/info' activeClassName={styles.activeLink}>Info</Link>
+                                <Link to='/info' activeClassName={styles.activeLink}>Info</Link>
                             </div>
                         </nav>
-                        <nav className={`${styles.verticalNavBottom} ${scrollPos > 0 && scrollDirection === 'down' ? styles.navBottomHidden : styles.navBottomActive}`}>
+                        <nav className={`${styles.verticalNavBottom} ${hasScrolled && scrollDirection === 'down' ? styles.navBottomHidden : styles.navBottomActive}`}>
                             <div className={styles.bottomRight}>
                                 <Button link='mailto:nveronicatran@gmail.com' className={styles.hideMobile}>nveronicatran@gmail.com</Button>
                                 <Link to='/fun' activeClassName={styles.activeLink} className={styles.showMobile}>Fun</Link>
@@ -72,10 +88,10 @@ class Nav extends React.Component {
 
                             <div className={styles.hideMobile}>
                                 <svg className={styles.icon} onClick={() => { this.handleClose(true); setActiveWindow('minesweeper'); newGame(); }} viewBox="0 0 24 24">
-                                <g>
-                                    <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/>
-                                </g>
-                            </svg>
+                                    <g>
+                                        <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+                                    </g>
+                                </svg>
                             </div>
 
                             <div className={`${styles.bottomLeft} ${styles.showMobile}`}>
